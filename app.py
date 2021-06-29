6703ee9cb1bc72bbd1acc2bb1a2bbe9d295c0260
moritzjuergens <moritzjuergens@outlook.de>
2021-06-29 20:17:32 +0200

    reincluded sanity route; fixed CORS error

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import *
 
 import os
-import res as res
 from flask_cors import CORS
+import res as res
 from werkzeug.utils import secure_filename
 
 
 key = os.urandom(24)
 app.secret_key = key
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 # allow front end
 # front end site   #https://hopeful-hermann-97c612.netlify.app/
-cors = CORS(app, resources={r"/": {"origins": "*"}})
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 # allow only pdf
 ALLOWED_EXTENSIONS = {'pdf'}
+
+# sanity check route
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 
 def allowed_file(filename):
@@ -27,7 +35,8 @@ def allowed_file(filename):
 @app.route('/')
 def hello():
     # front end site
-    return redirect("https://hopeful-hermann-97c612.netlify.app/", code=302)
+    # return redirect("https://hopeful-hermann-97c612.netlify.app/", code=302)
+    return redirect('/ping', code=302)
 
 
 UPLOAD_FOLDER = 'uploaded_files'
@@ -45,32 +54,38 @@ def run():
     print(' * received form with', list(request.form.items()))
     # get the uploaded file
     for uploaded_file in request.files.getlist('files'):
-        print("* recieved: " + uploaded_file.filename)
+        print(' * recieved:', uploaded_file.filename)
         if uploaded_file and uploaded_file.filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS:
             filename = secure_filename(uploaded_file.filename)
             uploaded_file.save(os.path.join(
                 app.config['UPLOAD_FOLDER'], filename))
-            print('* file uploaded', filename)
+            print(' * file uploaded', filename)
 
-            uploaded_file.save(uploaded_file.filename)
+            # uploaded_file.save(uploaded_file.filename)
             #os.rename(uploaded_file.filename, 'cv.pdf')
             # delte old json content
-            res.cleaning_json()
+
+            # res.cleaning_json()
+
             # convert pdf to json
-            res.pdf_to_json(uploaded_file.filename)
+            # res.pdf_to_json(uploaded_file.filename)
             # knn classifier
             # res.run()
-            category = res.run()
-            res.get_job()
+            #category = res.run()
+            # res.get_job()
         # test.cleaning_json()
-    return render_template("layout.html", result=category)
+    return 'Success'
 
 
 # get the jobs of  the result catogray and return as json structure for the front end
 @app.route('/jobs')
-def getjOB():
+def getjOB(jsonfile="data.json"):
     #post_data = request.get_json()
-    return res.get_job()
+    # return res.get_job()
+    f = open(jsonfile)
+    data = json.load(f)
+
+    return data
 
 
 if __name__ == '__main__':

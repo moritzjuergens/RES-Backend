9099ef5e83fd9f86b2f12daad797ee044eb495c2
moritzjuergens <moritzjuergens@outlook.de>
2021-06-29 15:55:01 +0200

    fixed file upload

--- app.py
+++ app.py
@@ -10,51 +10,68 @@ key = os.urandom(24)
 app.secret_key = key
 app = Flask(__name__)
 
-#allow front end           
-cors = CORS(app, resources={r"/": {"origins": "https://hopeful-hermann-97c612.netlify.app/"}})   #front end site   
+# allow front end
+# front end site   #https://hopeful-hermann-97c612.netlify.app/
+cors = CORS(app, resources={r"/": {"origins": "*"}})
 
-#allow only pdf 
+# allow only pdf
 ALLOWED_EXTENSIONS = {'pdf'}
+
+
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-
-
-#home route => redirect to front end
+# home route => redirect to front end
 @app.route('/')
 def hello():
-    return redirect("https://hopeful-hermann-97c612.netlify.app/", code=302)    #front end site
+    # front end site
+    return redirect("https://hopeful-hermann-97c612.netlify.app/", code=302)
 
 
-#send => run our code (algor) 
+UPLOAD_FOLDER = 'uploaded_files'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+# send => run our code (algor)
 @app.route('/send', methods=['POST'])
-#@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+# @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def run():
-    #get the uploaded file
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-        #os.rename(uploaded_file.filename, 'cv.pdf')
-        #delte old json content 
-        res.cleaning_json()
-        #convert pdf to json
-        res.pdf_to_json(uploaded_file.filename)
-        #knn classifier
-        #res.run()
-        category = res.run()
-        res.get_job()
-        #test.cleaning_json()
-    return render_template("layout.html", result = category) 
+    print(' * received form with', list(request.form.items()))
+    # get the uploaded file
+    for uploaded_file in request.files.getlist('files'):
+        print("* recieved: " + uploaded_file.filename)
+        if uploaded_file and uploaded_file.filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS:
+            filename = secure_filename(uploaded_file.filename)
+            uploaded_file.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename))
+            print('* file uploaded', filename)
+
+            uploaded_file.save(uploaded_file.filename)
+            #os.rename(uploaded_file.filename, 'cv.pdf')
+            # delte old json content
+            res.cleaning_json()
+            # convert pdf to json
+            res.pdf_to_json(uploaded_file.filename)
+            # knn classifier
+            # res.run()
+            category = res.run()
+            res.get_job()
+        # test.cleaning_json()
+    return render_template("layout.html", result=category)
 
 
-
-#get the jobs of  the result catogray and return as json structure for the front end
+# get the jobs of  the result catogray and return as json structure for the front end
 @app.route('/jobs')
 def getjOB():
     #post_data = request.get_json()
     return res.get_job()
 
+
 if __name__ == '__main__':
-    app.run(debug = True)  
+    app.run(debug=True)
--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/moritzjurgens/res/backend/env"
+set -gx VIRTUAL_ENV "/Users/moritzjurgens/res/res-backend/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
